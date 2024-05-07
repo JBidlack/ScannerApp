c591aa6e062fd4b27c4f1ace65d4a55d850ccda8
John <jbidlack@ccbcmd.edu>
2024-05-07 14:55:02 -0400

    Started writing Scanner class. Waiting on Justin to get me specs for the scanner

---
+++ pprd/src/main/java/app/Scanner.java
@@ -0,0 +1,34 @@
+package app;
+
+import javax.usb.UsbDevice;
+import javax.usb.UsbException;
+import javax.usb.UsbHostManager;
+import javax.usb.UsbHub;
+import javax.usb.UsbServices;
+
+public class Scanner {
+    public Scanner(){
+        try {
+            UsbServices service = UsbHostManager.getUsbServices();
+
+            UsbHub root = service.getRootUsbHub();
+
+            UsbDevice scanner = findDevice(root);
+
+        } catch (SecurityException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (UsbException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+    }
+
+    private static UsbDevice finDevice(UsbHub root){
+
+        return null;
+    }
+    
+    
+}
